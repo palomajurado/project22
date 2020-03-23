e5fc4281ed96cdd15ced243517138db66933e1ac
VivianaGuerraCustodio <60787174+VivianaGuerraCustodio@users.noreply.github.com>
2020-03-23 09:48:52 -0500

    Develop in (#6)
    * probando
    
    * prueba2
    
    * ...
    
    * cambios guardados
    
    * Cambios borrados
    
    * UI al 80%
    
    * seeData
    
    * seeData
    
    * funciones
    
    * cambios recuperados
    
    * n
    
    * funcion filtrar por roles
    
    * ver data
    
    * ...
    
    * cambios por Viviana
    
    * Funcion filtrar
    
    * ...
    
    * funcion filtar por roles listo
    
    * ...
    
    * readMe
    
    * README
    
    * Funcion roles y botones habilitados
    
    * nueva prueba
    
    Co-authored-by: Rossevv <ros22ch.caceres@gmail.com>
    Co-authored-by: Rossevv <55680887+Rossevv@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,64 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+/* interacción con el DOM event listeners o event handlers, se usan las que estàn en data.js */
+import {
+  allSelection,
+} from './data.js';
+import lol from './data/lol/lol.js';
 
-console.log(example, data);
+const allChampionList = lol.data;
+// console.log(allchampionList);
+
+// console.log(root);
+const divContador = document.getElementById('contador');
+const buttonAllChampions = document.getElementById('button_colection');
+
+buttonAllChampions.addEventListener('click', () => {
+  const list = document.querySelector('#root');
+  list.innerHTML = ''; /* Primero lo vacía luego coloca todo a todos los campeones */
+  Object.values(allChampionList).forEach((champion) => {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    p.className = 'nameOfChampion';
+    img.className = 'imageOfChampion';
+    p.innerHTML = `${champion.name}`;
+    img.src = `${champion.splash}`;
+    div.appendChild(img);
+    div.appendChild(p);
+    list.appendChild(div);
+    divContador.innerHTML = 'Todos los campeones 122';
+  });
+});
+
+const liRoles = document.querySelectorAll('.Roles');
+// console.log(typeof liRoles); /*is an object*/
+liRoles.forEach((option) => {
+  option.addEventListener('click', () => {
+    // const functionRoles = () => {
+    const typeRol = option.getAttribute('data-value');
+    // console.log(typeRol);
+    const functionFilterRol = allSelection(allChampionList, typeRol);
+    // console.log(functionFilterRol);
+    const list = document.querySelector('#root');
+    list.innerHTML = '';
+    /* Llamamos otra vez al div que contiene list,la limpiamos=> pinta lo nuevo que vamos a darle */
+    // console.log(root);
+    Object.values(functionFilterRol).forEach((champion) => {
+      /* se busca el valor=>volvemos usar object.values y acceder a los valores de lo importado */
+      const div = document.createElement('div');
+      const img = document.createElement('img');
+      const p = document.createElement('p');
+      const p2 = document.createElement('p');
+      p.className = 'nameOfChampion';
+      p2.className = 'tagsOfChampion';
+      img.className = 'imageOfChampion';
+      p.innerHTML = `${champion.name}`;
+      p2.innerHTML = `${champion.tags}`;
+      img.src = `${champion.splash}`;
+      div.appendChild(img);
+      div.appendChild(p);
+      div.appendChild(p2);
+      list.appendChild(div);
+    });
+    divContador.innerHTML = (`${typeRol} ${functionFilterRol.length}`);
+  });
+});
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,39 @@
-import { example, anotherExample } from '../src/data.js';
+import {
+  allSelection,
+// eslint-disable-next-line import/no-unresolved
+} from '../src/data.js';
 
+import lol from '../src/data/lol/lol.js';
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+const champions = lol.data;
+
+describe('allSelection', () => {
+  it('debería ser una función', () => {
+    expect(typeof allSelection).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('debería retornar rol Mago', () => {
+    const filterRol = allSelection(champions, 'Mage');
+    expect(filterRol).toHaveLength(52);
   });
 });
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
+it('debería retornar rol Tank', () => {
+  const filterRol = allSelection(champions, 'Tank');
+  expect(filterRol).toHaveLength(40);
+});
+it('debería retornar rol Fighter', () => {
+  const filterRol = allSelection(champions, 'Fighter');
+  expect(filterRol).toHaveLength(66);
+});
+it('debería retornar rol Apoyo', () => {
+  const filterRol = allSelection(champions, 'Support');
+  expect(filterRol).toHaveLength(27);
+});
+it('debería retornar rol Tirador', () => {
+  const filterRol = allSelection(champions, 'Marksman');
+  expect(filterRol).toHaveLength(24);
+});
+it('debería retornar rol Asesino', () => {
+  const filterRol = allSelection(champions, 'Assassin');
+  expect(filterRol).toHaveLength(33);
 });
